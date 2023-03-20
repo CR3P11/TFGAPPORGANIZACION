930b146f09ae8cbfdf73cd99237e809521a2b183
Crepy <crepyland@gmail.com>
2023-03-20 10:52:01 +0100

    Se añaden cambios de pantalla dentro de cada clase

--- OrganizadorAPP.py
+++ OrganizadorAPP.py
@@ -29,31 +29,36 @@ class Organizador(App):
     screen_manager = ScreenManager()
 
     # Agregar las pantallas Acceder y Registro al ScreenManager
-    
-    
 
-    
+
 
 
 
     
     def build(self,screen_manager=screen_manager):
         
-        
         #Este es el primer metodo que se ejecuta en una app de kivy
         
         Builder.load_file('Diseño.kv')
         
-        self.InicializarScreens(screen_manager)
+        screen_manager = self.InicializarScreens(screen_manager)
+
+        
+
+        self.url="https://organizador-5de77-default-rtdb.europe-west1.firebasedatabase.app/.json"
+        self.key= "DDG9a9IkOp9ZtvtbhmU0BPJacoBfSxsb4KglypP6"
         
         screen_manager.current = 'acceder'
+
+        
+
         Window.size = (400, 600)
         
         return screen_manager
     
-
     
-    def InicializarScreens( screen_manager):
+    
+    def InicializarScreens(self,screen_manager):
         acceder_screen = Screen(name='acceder')
         acceder_screen.add_widget(Acceder())
         screen_manager.add_widget(acceder_screen)
@@ -62,24 +67,49 @@ class Organizador(App):
         registro_screen.add_widget(Registro())
         screen_manager.add_widget(registro_screen)
         
+
+
         return screen_manager
         
     
 
-    def PantallaRegistro(self,screen_manager=screen_manager):
-        
-        screen_manager.current = 'registro'
+    def register_user(self, username_input,email_input,password_input):
+        logger.info('Tu usuario es: ' + str(username_input)+' tu email: '+str(email_input)+' y tu contraseña: '+ str(password_input))
 
 
-
+ 
 
 class Registro(Screen):
-    None
+    screen_manager = Organizador().screen_manager
+    def PantallaInicio(self,screen_manager=screen_manager):
+        #Esta clase se ejecuta para cambiar de la pantalla registro a acceder, elimina los campos para que 
+        #Vuelvan a quedar en blanco.
+        
+        self.ids.password_input.text = ''
+        self.ids.email_input.text = ''
+        self.ids.username_input.text = ''
+        
+        # logger.info('Entra')
+        
+        
+        screen_manager.current = 'acceder'
+        return screen_manager
+
 class Acceder(Screen):
+    screen_manager = Organizador().screen_manager
     
-    None
+    def PantallaRegistro(self,screen_manager=screen_manager):
+        #Esta clase se ejecuta para cambiar de la pantalla acceder a registro, elimina los campos para que 
+        #Vuelvan a quedar en blanco.
+        self.ids.username_input.text = ''
+        self.ids.password_input.text = ''
+
+
+        screen_manager.current = 'registro'
+
 if __name__ == '__main__':
     try:
         Organizador().run()
+
     except Exception as e:
         logger.error(e)
