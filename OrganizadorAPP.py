dfcecb3141b9d866a69fda79d331d11b939c8b29
Crepy <crepyland@gmail.com>
2023-05-09 16:52:12 +0200

    avance mochila y ajustes

--- OrganizadorAPP.py
+++ OrganizadorAPP.py
@@ -12,6 +12,7 @@ pip install kivy
 pip install requests
 pip install validate_email
 
+
 Todas las librerias son necesarias para el correcto funcionamiento de la aplicación.
 
 '''
@@ -27,7 +28,14 @@ from kivy.properties import ListProperty
 from kivy.uix.label import *
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-
+from kivy.metrics import dp
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.graphics import Color, Rectangle
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
@@ -45,7 +53,7 @@ class Organizador(App):
 
     
     screen_manager = ScreenManager()
-
+    
     # Agregar las pantallas Acceder y Registro al ScreenManager
 
 
@@ -64,20 +72,23 @@ class Organizador(App):
         screen_manager = self.InicializarScreens(screen_manager)
 
         
+        self.ColorCanvas("verde", True)
 
         
         # screen_manager.current = 'acceder'
-        screen_manager.current = 'principal'
+        # screen_manager.current = 'principal'
 
         
 
         Window.size = (400, 600)
+
+
         
         return screen_manager
     
     def pantallaPrincipal(self):
         self.screen_manager.current = 'principal'
-    
+        
     def InicializarScreens(self,screen_manager):
         acceder_screen = Screen(name='acceder')
         acceder_screen.add_widget(Acceder())
@@ -91,9 +102,9 @@ class Organizador(App):
         principal_screen.add_widget(PantallaPrincipal())
         screen_manager.add_widget(principal_screen)
         
-        calendar_screen = Screen(name='calendario')
-        calendar_screen.add_widget(PantallaCalendario())
-        screen_manager.add_widget(calendar_screen)
+        settings_screen = Screen(name='ajustes')
+        settings_screen.add_widget(PantallaAjustes())
+        screen_manager.add_widget(settings_screen)
 
         contact_screen = Screen(name='contactos')
         contact_screen.add_widget(PantallaContactos())
@@ -102,10 +113,6 @@ class Organizador(App):
         backpack_screen = Screen(name='mochila')
         backpack_screen.add_widget(PantallaMochila())
         screen_manager.add_widget(backpack_screen)
-
-        settings_screen = Screen(name='ajustes')
-        settings_screen.add_widget(PantallaAjustes())
-        screen_manager.add_widget(settings_screen)
 
         notes_screen = Screen(name='notas')
         notes_screen.add_widget(PantallaNotas())
@@ -118,10 +125,34 @@ class Organizador(App):
 
 
         return screen_manager
-        
     
+    def ColorCanvas(self, color,dentro=True):
+        colors = {
+            "amarillo": (1, 1, 0),
+            "verde": (0, 1, 0),
+            "rojo": (1, 0, 0),
+            "naranja": (1, 0.5, 0),
+            "morado": (0.5, 0, 1),
+            "azul": (0, 0, 1),
+            "cian": (0, 1, 1),
+            "gris": (0.5, 0.5, 0.5),
+            "blanco": (1, 1, 1),
+            "negro": (0, 0, 0)
+        }
+        self.screen_manager.canvas.add(Color(*colors[color]))
+        self.screen_manager.canvas.add(Rectangle(size=(400, 600)))
+        Builder.load_file('Diseño.kv')
+
+        if dentro == True:
+            self.screen_manager.current = 'principal'
+        elif dentro==False:
+            self.screen_manager.current = 'registro'
+            self.screen_manager.current = 'acceder'
+        # else:
+        #     self.screen_manager.current = 'error'
 class Registro(Screen):
     screen_manager = Organizador().screen_manager
+    
     def PantallaInicio(self,screen_manager=screen_manager):
         #Esta clase se ejecuta para cambiar de la pantalla registro a acceder, elimina los campos para que 
         #Vuelvan a quedar en blanco.
@@ -225,6 +256,8 @@ class Acceder(Screen):
 
 class PantallaPrincipal(Screen):
     screen_manager = Organizador().screen_manager
+
+
     def pantallanotas(self):
         self.screen_manager.current = 'notas'
     def pantallacontactos(self):
@@ -233,10 +266,8 @@ class PantallaPrincipal(Screen):
         self.screen_manager.current = 'mochila'
     def pantallaAjustes(self):
         self.screen_manager.current = 'ajustes'
-    def pantallacalendario(self):
-        self.screen_manager.current = 'calendario'
-
-
+    def pantallaAcceder(self):
+        self.screen_manager.current = 'acceder'
 
 
 
@@ -285,26 +316,213 @@ class PantallaNotas(Screen):
 
 
 class PantallaMochila(Screen):
+    mochilas = {}
+    screen_manager = Organizador().screen_manager
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        # Cargar las mochilas desde el archivo JSON
+        with open('mochilas.json', 'r') as f:
+            self.mochilas = json.load(f)
+        
+        # Crear los botones de las mochilas
+        mochilas_layout = self.ids.mochilas_layout
+        for nombre_mochila, items in self.mochilas.items():
+            mochila_button = Button(
+                text=nombre_mochila,
+                size_hint_y=None,
+                height=dp(40),
+                on_release=lambda button: self.mostrar_items_mochila(button.text)
+            )
+            mochilas_layout.add_widget(mochila_button)
+            mochilas_layout.height += mochila_button.height + mochilas_layout.spacing[1]
 
-    def mochilas():
-        pass
-    def actualizar_mochila():
-        pass
+    def añadir_mochila(self):
+        nombre_mochila = self.ids.mochila.text.strip()
+        if nombre_mochila:
+            self.mochilas[nombre_mochila] = []
+            self.ids.mochila.text = ""
+            mochilas_layout = self.ids.mochilas_layout
+            mochila_button = Button(
+                text=nombre_mochila,
+                size_hint_y=None,
+                height=dp(40),
+                on_release=lambda button: self.mostrar_items_mochila(button.text)
+            )
+            self.ids.mochilas_container.add_widget(mochila_button)
+            self.ids.mochilas_container.height += mochila_button.height + mochilas_layout.spacing[1]
+            self.ids.mochila.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+            self.ids.plus_button.pos_hint = {"center_x": 0.5, "center_y": 0.4}
+
+            with open('mochilas.json', 'w') as f:
+                json.dump(self.mochilas, f)
+    
+    def mostrar_items_mochila(self, nombre_mochila):
+        items_screen = Screen(name='items')
+        items_screen.add_widget(Items(nombre_mochila,self.mochilas[nombre_mochila]))
+        self.screen_manager.add_widget(items_screen)
+        
+        self.screen_manager.current = 'items'
+class Items(Screen):
+    screen_manager = Organizador().screen_manager
+    items_layout = GridLayout(cols=1, spacing=10, size_hint_y=1)
+    
+    def __init__(self, mochila, items):
+        # Guardar el nombre de la mochila en un atributo
+        self.nombre_mochila = mochila
+        
+        # Crear un GridLayout para los items
+        self.items_layout.bind(minimum_height=self.items_layout.setter('height'))
+       
 
 
 
-    def borrar(self):
-        pass
+        # Añadir un Label con el nombre de la mochila
+        mochila_label = Label(text=mochila, font_size=15, bold=True, size_hint=(1, 0.1), pos_hint={"x": 0, "top": 0.95})
+
+        space = Label(text="", size_hint_y=None, height=dp(70))
+        self.items_layout.add_widget(space)
+        
+        if items:
+            # Añadir los botones de los items
+            for item in items:
+                item_button = Button(text=item, size_hint_y=None, height=dp(25),on_release=lambda event, nombre_item=item:self.eliminarItem(nombre_item, mochila,scroll_view, self.items_layout))
+                self.items_layout.add_widget(item_button)
+        else:
+            # Agregar un widget de relleno para asegurarse de que el GridLayout tenga una altura mínima
+            filler = Label(text="No hay objetos", size_hint_y=None, height=dp(25))
+            self.items_layout.add_widget(filler)
+        
+        
+
+        self.items_layout.height = len(items) * (dp(40) + self.items_layout.spacing[1])
+   
+        # Añadir el ScrollView que contendrá los botones de los items
+        scroll_view = ScrollView()
+        scroll_view.add_widget(self.items_layout)
+        super().__init__()
+        self.add_widget(mochila_label)
+        self.add_widget(scroll_view)
+        self.add_widget(Button(text="<", size_hint=(0.15, 0.05), pos_hint={"x": 0, "top": 0.98}, on_release=lambda event: self.pantallaAnterior(scroll_view,self.items_layout)))
+        self.add_widget(Button(text="+", size_hint=(0.15, 0.05), pos_hint={"x": 0.80, "top": 0.98}, on_release=lambda event: self.AñadirItems(mochila,scroll_view,self.items_layout)))
+    
+    def eliminarItem(self, nombre_item, mochila,scroll_view, items_layout):
+        # Obtener la lista actual de items de la mochila
+        mochilas = json.load(open("mochilas.json", "r"))
+        items = mochilas[mochila]
+        # Eliminar el item de la lista
+        items.remove(nombre_item)
+        # Actualizar el JSON
+        mochilas[mochila] = items
+        with open("mochilas.json", "w") as f:
+            json.dump(mochilas, f)
+        # Actualizar la pantalla de items
+        items_layout.clear_widgets()
+        self.borrar(scroll_view,items_layout)
+        self.__init__(mochila, items)
+
+        
+    
+    
+    def pantallaAnterior(self,scroll_view,items_layout):
+        self.screen_manager.current = 'mochila'
+        self.screen_manager.remove_widget(self.screen_manager.get_screen('items'))
+        self.borrar(scroll_view,items_layout)
+
+
+    def AñadirItems(self,mochila,scroll_view,items_layout):
+        # Crear un PopUp para introducir el nombre del nuevo item
+        content = BoxLayout(orientation='vertical', spacing=dp(10))
+        nombre_label = Label(text="Nombre del item:")
+        nombre_input = TextInput(multiline=False)
+        content.add_widget(nombre_label)
+        content.add_widget(nombre_input)
+        popup = Popup(title="Agregar item", content=content, size_hint=(0.5, 0.5))
+        button_layout = GridLayout(cols=2, spacing=dp(10), size_hint_y=None, height=dp(40))
+        cancel_button = Button(text="Cancelar", on_release=popup.dismiss)
+        agregar_button = Button(text="Agregar", on_release=lambda event: self.agregarItem(nombre_input.text, mochila, popup,scroll_view,items_layout))
+        button_layout.add_widget(cancel_button)
+        button_layout.add_widget(agregar_button)
+        content.add_widget(button_layout)
+        
+        popup.open()
+   
+    def agregarItem(self, nombre_item, mochila, popup,scroll_view,items_layout):
+
+        
+        if len(nombre_item)<1 or len(nombre_item)>40:
+            nombre_item = 'error caracteres'
+        
+        # Obtener la lista actual de items de la mochila
+        mochilas = json.load(open("mochilas.json", "r"))
+        items = mochilas[mochila]
+        # Agregar el nuevo item a la lista
+        items.append(nombre_item)
+        # Actualizar el JSON
+        mochilas[mochila] = items
+        with open("mochilas.json", "w") as f:
+            json.dump(mochilas, f)
+        # Actualizar la pantalla de items
+        self.borrar(scroll_view,items_layout)
+        self.__init__(mochila, items)
+        popup.dismiss()
+
+    def borrar(self,scroll_view,items_layout):
+        try:
+            if scroll_view is not None:
+                for widget in scroll_view.children[:]:
+                    scroll_view.remove_widget(widget)
+            if items_layout is not None:
+                for widget in items_layout.children[:]:
+                    items_layout.remove_widget(widget)
+                    
+        except AttributeError:
+            pass    
 
 class PantallaContactos(Screen):
     None
-class PantallaCalendario(Screen):
-    None
+
+
+
+        
 class PantallaAjustes(Screen):
-    None
+    screen_manager = Organizador().screen_manager
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def color(self):
+        color_screen = Screen(name='color')
+        color_screen.add_widget(SelectColor())
+        self.screen_manager.add_widget(color_screen)
+
+        self.screen_manager.current = 'color'
+
+class SelectColor(Screen):
+    screen_manager = Organizador().screen_manager
+    def set_background_color(self, color):
+        color_map = {"amarillo": [1, 1, 0, 1],
+                    "verde": [0, 1, 0, 1],
+                    "rojo": [1, 0, 0, 1],
+                    "naranja": [1, 0.5, 0, 1],
+                    "morado": [0.5, 0, 1, 1],
+                    "azul": [0, 0, 1, 1],
+                    "cian": [0, 1, 1, 1],
+                    "gris": [0.5, 0.5, 0.5, 1],
+                    "blanco": [1, 1, 1, 1],
+                    "negro": [0, 0, 0, 1]}
+        color_rgba = color_map.get(color)
+        
+        self.screen_manager.canvas.add(Color(*color_rgba))
 
 
-
+class CustomScreenManager(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Reemplazar las pantallas existentes con instancias de ColorScreen modificadas
+        for name, screen in self.screens.items():
+            self.remove_widget(screen)
+            self.add_widget(ColorScreen(name=name))
+            
 class ContenidoScreen(Screen):
     screen_manager = Organizador().screen_manager
 
@@ -389,6 +607,7 @@ class PantallaMostrarNotas(Screen):
 
 if __name__ == '__main__':
     try:
+        
         Organizador().run()
 
     except Exception as e:
